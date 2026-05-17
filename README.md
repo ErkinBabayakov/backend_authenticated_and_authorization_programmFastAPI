36f9a09531dda0f10e5e3e43168e8f650b67b63f
Erkin Babayakov <erkinbabayakov@yandex.ru>
2026-05-17 21:26:47 +0300

    Изменил main.py

--- README.md
+++
@@ -1,4 +0,0 @@
-git config --local user.name "Erkin Babayakov"
-git config --local user.email "erkinbabayakov@yandex.ru"
-
-git remote add gitlab git@gitlab.com:Freeyourself/users_authenticated_programm.git
--- src/main.py
+++ src/main.py
@@ -19,10 +19,12 @@ app.include_router(auth_router)
 @app.get("/", response_class=HTMLResponse, tags=["Главная страница документации"])
 def home():
     return """
-    <h2><a href="http://127.0.0.1:8001/docs">Documentation</a><br></h2>
-    <h2><a href="http://127.0.0.1:8001/redoc">ReDoc</a></h2>
+    <h1>Выберите тип документации</h1>
+    <h2><a href="https://booking-fastapi-project.ru/docs">Swagger UI</a><br></h2>
+    <h2><a href="https://booking-fastapi-project.ru/redoc">ReDoc</a></h2>
     """
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+
